913530bd4bdae56e19e3ae254f7d5c4a4c6b1bca
Eduard Korogodov <eduard.korogodov@gmail.com>
2018-07-16 00:36:24 +0300

    reorganize file structure

--- Source/TestingGroundsFPSUE4/TestingGroundsFPSUE4GameMode.cpp
+++ Source/TestingGroundsFPSUE4/TestingGroundsFPSUE4GameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "TestingGroundsFPSUE4GameMode.h"
 #include "TestingGroundsFPSUE4HUD.h"
-#include "TestingGroundsFPSUE4Character.h"
+#include "FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 ATestingGroundsFPSUE4GameMode::ATestingGroundsFPSUE4GameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
